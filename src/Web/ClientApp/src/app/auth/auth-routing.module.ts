1240cd3552261c151bee60d7355c23c0c0609273
Yassine Bayoudh <bayoudh.yassine20@gmail.com>
2024-03-08 19:37:54 +0100

    feature : add authentication pages

--- src/Web/ClientApp/src/app/auth/auth-routing.module.ts
+++ src/Web/ClientApp/src/app/auth/auth-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
+  { path: 'error', loadChildren: () => import('./error/error.module').then((m) => m.ErrorModule) },
+  { path: 'access', loadChildren: () => import('./access/access.module').then((m) => m.AccessModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
+  { path: '**', redirectTo: '/notfound' },
 ];
 
 @NgModule({

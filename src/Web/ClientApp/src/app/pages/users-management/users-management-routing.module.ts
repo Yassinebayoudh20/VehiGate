5f3f402bd4e3931fa45d485f680e419eee594cea
Yassine Bayoudh <bayoudh.yassine20@gmail.com>
2024-03-13 15:55:03 +0100

    feature : implement user form

--- src/Web/ClientApp/src/app/pages/users-management/users-management-routing.module.ts
+++ src/Web/ClientApp/src/app/pages/users-management/users-management-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './users-list/users-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 const routes: Routes = [
-  { path: '', component: UsersListComponent }
+  { path: '', component: UsersListComponent },
+  { path: 'upsert/:id', component: UserFormComponent },
+  { path: 'upsert', component: UserFormComponent },
 ];
 
 @NgModule({
--- src/Web/ClientApp/src/app/pages/users-management/users-management.module.ts
+++ src/Web/ClientApp/src/app/pages/users-management/users-management.module.ts
@@ -6,18 +6,18 @@ import { UsersListComponent } from './users-list/users-list.component';
 import { UsersComponent } from './users.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { UserFormComponent } from './user-form/user-form.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { TranslocoModule } from '@ngneat/transloco';
+import { ReactiveFormsModule } from '@angular/forms';
 
+const PRIME_UI_MODULES = [DropdownModule, InputTextModule, ButtonModule, PasswordModule];
 
 @NgModule({
-  declarations: [
-    UsersComponent,
-    UsersListComponent
-  ],
-  imports: [
-    CommonModule,
-    CoreModule,
-    SharedModule,
-    UsersManagementRoutingModule
-  ]
+  declarations: [UsersComponent, UsersListComponent, UserFormComponent],
+  imports: [CommonModule, CoreModule, TranslocoModule, SharedModule,ReactiveFormsModule, UsersManagementRoutingModule, ...PRIME_UI_MODULES],
 })
-export class UsersManagementModule { }
+export class UsersManagementModule {}

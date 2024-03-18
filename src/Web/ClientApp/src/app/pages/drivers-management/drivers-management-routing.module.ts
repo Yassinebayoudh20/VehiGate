461ce762a2c20be0059dd87b0dda18f71c24534b
Nehed RIAHI <n.riahi@case-tunisia.com>
2024-03-18 23:37:02 +0100

    feat: add drivers mangment

--- src/Web/ClientApp/src/app/pages/drivers-management/drivers-management-routing.module.ts
+++ src/Web/ClientApp/src/app/pages/drivers-management/drivers-management-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DriverFormComponent } from './driver-form/driver-form.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:DriversListComponent },
+  { path: 'upsert/:id', component: DriverFormComponent },
+  { path: 'upsert', component: DriverFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
